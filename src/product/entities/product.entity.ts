4a50492f5b28c4283475e2f1566290b67c74d3df
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2023-09-13 20:17:08 -0300

    price agora é NUMERIC(10,2)

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -27,7 +27,7 @@ export class ProductEntity {
   @Column()
   description: string;
 
-  @Column()
+  @Column('numeric', { precision: 10, scale: 2 })
   price: number;
 
   @Column({ enum: ProductType })
